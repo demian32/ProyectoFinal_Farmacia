b7600a175a02e3371bc19ab4b67a966c46b10352
Deyanira <Deyanira@Deyanira.lan>
2014-04-17 08:48:24 -0500

    Servlet

---
+++ Farmacia_1/src/java/com/jc/controladores/ServletBuscartodo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jc.controladores;
+
+import com.jc.model.BuscarTodo;
+import com.jc.model.Medicamento;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletBuscartodo extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            for(Medicamento u:BuscarTodo.buscarTodos()){   
+                out.println(u.getID_medicamento());
+            }
+        }catch(Exception ex){
+                    
+                    }
+    }
+}
+        
+            
